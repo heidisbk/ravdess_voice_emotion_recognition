d4fb75ea1b1c3904399db8d57d2237002ff463d4
manalbouffa <manalbouffa@gmail.com>
2025-01-16 12:46:39 +0100

    Create model_from_scratch.py

---
+++ scripts/model_from_scratch.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+
+
+file_path = "/content/advanced_processed_data.csv"  
+data = pd.read_csv(file_path)
+
+X = data.iloc[:, :-2] 
+y = data['label_encoded']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+model = Sequential()
+model.add(Dense(256, input_dim=X_train.shape[1], activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(y_train.shape[1], activation='softmax'))
+
+model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=500, batch_size=32, validation_split=0.2, verbose=1)
+
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print(f"Test Accuracy: {accuracy:.2f}")
+
+
+import pickle
+with open("model.pkl", "wb") as model_file:
+    pickle.dump(model, model_file)
+with open("scaler.pkl", "wb") as scaler_file:
+    pickle.dump(scaler, scaler_file)
+
+print("Modèle de réseau de neurones sauvegardé sous 'model.pkl'.")
